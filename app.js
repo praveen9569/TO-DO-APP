12161a5b3d9ef274883f6128c50bbc95fdc44009
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-08-05 05:57:04 +0000

    added some css background

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
-document.querySelector('#push').onclick = function() {
+document.querySelector("#push").onclick = function () {
+    const taskInput = document.querySelector('#newtask input');
+    const taskText = taskInput.value.trim();
 
-    if (document.querySelector('#newtask input').value.length == 0) {
-        alert("Please enter a task");
-    } else {
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
-
-        // Clear the input field after adding the task
-        document.querySelector('#newtask input').value = '';
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for (var i = 0; i < current_tasks.length; i++) {
-            current_tasks[i].onclick = function() {
-                this.parentNode.remove(); // Use remove() to delete the task
-            };
-        }
-
-        var tasks = document.querySelectorAll(".task");
-        for (var i = 0; i < tasks.length; i++) {
-            tasks[i].onclick = function() {
-                this.classList.toggle('completed');
-            };
-        }
+    if (taskText.length === 0) {
+        alert("Please enter a task!");
+        return;
     }
+
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('task');
+
+    taskContainer.innerHTML = `
+        <span>${taskText}</span>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    `;
+
+    document.querySelector('#tasks').appendChild(taskContainer);
+
+    taskInput.value = '';
+
+    taskContainer.querySelector('.delete').onclick = function () {
+        this.parentElement.remove();
+    };
 };
